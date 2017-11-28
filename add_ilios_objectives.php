b2246554881cf6e664a3c4480ac96dd89c885386
Jason Hedrick <thecoolestguy@gmail.com>
2017-11-27 16:17:02 -0800

    cleanup

--- add_ilios_objectives.php
+++ add_ilios_objectives.php
@@ -3,12 +3,13 @@
  * Really simple example creating program-year objectives (and link it to a competency while at it)
  * via the Ilios3 API.
  *
- * Visit https://ilios3-demo.ucsf.edu/programs/1/programyears/1 to view results
+ * Visit [YOUR ILIOS API HOST]/programs/1/programyears/1 to view the results
+ * (eg, https://ilios3-demo.ucsf.edu/programs/1/programyears/1 )
  */
 
 // First, set up some constant variables...
 // API_TOKEN: If you don't already have one, you'll need a JSON Web Token (JWT) to authenticate!
-// To learn how to create/test a token, please visit https://github.com/ilios/ilios/blob/master/ILIOS_API.md#creating-a-json-web-token-jwt
+// To learn how to create/test a token, please visit https://github.com/ilios/ilios/blob/master/docs/ilios_api.md#creating-a-json-web-token-jwt
 const API_TOKEN = 'YOUR TOKEN GOES HERE'; // put your JWT token here.
 
 // API_HOST: This is the url to the Ilios instance you are working on, just enter it as you normally would in a browser
@@ -17,48 +18,58 @@ const API_HOST = 'https://ilios3-demo.ucsf.edu';
 // be changed, but you can never know for sure, so we set it as a variable here: 
 const API_PATH = '/api/v1';
 
-// CSV_FILE: If you would like to parse a CSV/spreadsheet file for your data, set the path to the file on the system where you will be
-// executing this script if you'd rather use an array of manual-entered data, set this to value to 'false' (without the quotes!)
-const CSV_FILE = 'data/objectives.csv';
+// DATA_FILE: If you would like to parse a file for your data, set the path to the file on the system where you will be
+// executing this script.
+// If you'd rather use an array of manual-entered data, set this to value to 'false' (without the quotes!)
+const DATA_FILE = 'data/objectives.csv';
 
-// If we are planning to add objectives, we'll need to set append the 'objectives' endpoint to the url.  To learn which endpoints are 
-//available, append '/api/doc' to your API_HOST url or look at https://ilios3-demo.ucsf.edu/api/doc for reference.
-// Using the variables we set above, this line creates the $objectiveAPIEndpoint value as 'https://ilios3-demo.ucsf.edu/api/v1/objectives'
+// If you are planning to add objectives, we'll need to set append the `/objectives` endpoint to the url. To learn
+// which endpoints are available to use, simply append '/api/doc' to your API_HOST url or take a look at
+// https://ilios3-demo.ucsf.edu/api/doc for reference.
+
+// Using the variables we set above, this line creates the $objectiveAPIEndpoint value as
+// `https://ilios3-demo.ucsf.edu/api/v1/objectives`
 $objectiveAPIEndpoint = API_HOST . API_PATH . '/objectives';
 
-// If we chose to use a CSV file for our data (recommended!) and set the path to the file above, let's parse its data into an array
-if (false === CSV_FILE){
+// If you chose to use a CSV file for your data (recommended!) and have properly set the path to the file above, here is
+// where we parse its data into an array
+if (false !== DATA_FILE && is_file(DATA_FILE)){
+
+
     
 } else {    
-    //otherwise, let's create the arrays manually
+    //otherwise, for small changes/amounts of data, you can create the arrays manually instead of using a data file...
     $programYearId = 1;
     $competencyId = 115;
     $sessionId = 32980;
 
     $objectives = [
-        [ 'title' => 'Foo Bar', 'competency' =>  $competencyId, 'sessions' => [ $sessionId  ] ],
+        [ 'title' => 'Foo Bar', 'competency' =>  $competencyId, 'sessions' => [ $sessionId ] ],
         //[ 'title' => 'Lorem Ipsum', 'programYears' => [ $programYearId ], 'competency' =>  $competencyId]
     ];
 }
 
 // We are going to use the 'curl' application to make the call to the API, so we set up the 'Curl Handler' ($ch) here:
+
 // First we initialize the curl handle ($ch), by feeding it the objectives endpoint we created above.
 $ch = curl_init($objectiveAPIEndpoint);
 // Now that the curl handle/session is initialized, let's set some of its options
-// 1. As we can see in the docs at https://ilios3-demo.ucsf.edu/api/doc#section-Objective, creating a new objective requires
-// using the 'POST' method, so let's make sure we set the CURLOPT_CUSTOMREQUEST curl option to 'POST'
+// 1. As we can see in the docs at https://ilios3-demo.ucsf.edu/api/doc#section-Objective, creating a new objective
+// requires using the 'POST' method, so let's make sure we set the CURLOPT_CUSTOMREQUEST curl option to 'POST'
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-// 2. Because we'll want to parse the returned JSON data as an actual string of text, we set the RETURNTRANSFER option to true
+// 2. Because we'll want to parse the returned JSON data as an actual string of text, we set the RETURNTRANSFER option
+// to true
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// 3. We also want to make sure to track the headers sent in case of error(s)
+// 3. We want to make sure to track the headers sent in case of error(s), so set the CURINFO_HEADER_OUT value to true.
 curl_setopt($ch, CURLINFO_HEADER_OUT, true);
 
-// Whether we chose to use a CSV file for our data or we created it by hand, all of our data is now in the '$objectives' array.
-// So let's parse each item in the $objectives array one at a time via a 'foreach' loop, and add its fields/values to the curl 
-// handler for POSTing to the API via the curl handler ($ch)
+// Whether we chose to use a CSV file for our data or we created it by hand, all of our data is now in the `$objectives`
+// array. So let's parse each item in the $objectives array one at a time via a 'foreach' loop, and add its
+// fields/values to the curl handler ($ch) for POSTing to the API
 foreach ($objectives as $objective) {
 
-    //objectives need to be sent wrapped in an array named 'objectives', so let's wrap it here
+    // As shown at
+    // objectives need to be sent wrapped in an array named 'objectives', so let's wrap it here
     $objectives_payload = array('objectives' => array($objective));
     // Because the Ilios API works with programming language-agnostic 'JSON' data format, we first need to convert 
     // each record's fields/values into a JSON object. We do that here, putting the record's fieldnames/values into
